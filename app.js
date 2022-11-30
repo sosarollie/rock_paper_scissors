909c24b04bcf0bfb68367cd60f4edf0f496b0fe5
18praise <jesussosarollie1812@gmail.com>
2022-11-30 07:56:52 -0300

    Finish game

--- app.js
+++ app.js
@@ -1,30 +1,22 @@
 function getComputerChoice() {
-    const num = Math.floor(Math.random() * 3);
+    let randomNum = Math.floor(Math.random() * 3);
     let choice;
-    if (num == 0) {
-        choice = "Rock";
-    } else if (num == 1) {
-        choice = "Paper";
-    } else { 
-        choice = "Scissors";
+    switch (randomNum) {
+    case 0:
+        return "Rock"
+    case 1:
+        return "Paper"
+    case 2:
+        return "Scissors"
     }
-    return choice;
 }
-
-const computerSelection = getComputerChoice();
-
-let playerSelection = prompt("Choose! Rock, Paper or Scissors?");
 
 function capitalize(char) {
     const cap = char.charAt(0).toUpperCase() + char.slice(1).toLowerCase();
     return cap;
 }
 
-playerSelection = capitalize(playerSelection);
-
-console.log(playerSelection);
-
-function playRound (playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection, playerWins, computerWins) {
     let winner;
 
     if ((computerSelection == "Rock") && (playerSelection == "Scissors")){
@@ -46,15 +38,38 @@ function playRound (playerSelection, computerSelection) {
         winner = "Player";
     }
 
-    if (winner == "Computer"){
-        return "You Lose! " + computerSelection + " beats " + playerSelection; 
-    } 
-    else if (winner == "Player"){
-        return "You Win! " + playerSelection + " beats " + computerSelection; 
-    }
-    else {
-        return "It's a Draw!";
-    }
+    return winner;
 }
 
-console.log(playRound(playerSelection,computerSelection));
+function game(){
+    let computerSelection;
+    let playerSelection;
+    let playerWins = 0;
+    let computerWins = 0;
+    let winner;
+    for (let i = 0; i < 5; i++){
+        computerSelection = getComputerChoice();
+        playerSelection = prompt("Choose! Rock, Paper or Scissors?");
+        playerSelection = capitalize(playerSelection);
+        console.log(computerSelection);
+        winner = (playRound(playerSelection,computerSelection));
+        if (winner == "Computer"){
+            computerWins++;
+            console.log("You Lose! " + computerSelection + " beats " + playerSelection); 
+        } 
+        else if (winner == "Player"){
+            playerWins++;
+            console.log("You Win! " + playerSelection + " beats " + computerSelection); 
+        }
+        else {
+            console.log("It's a Draw!");
+        } 
+    }
+    if (computerWins > playerWins){
+        console.log("You lose the match! " + computerWins + " to " + playerWins);
+    }else if (computerWins < playerWins){
+        console.log("You Win the match! " + playerWins + " to " + computerWins);
+    }      
+}
+
+game();
